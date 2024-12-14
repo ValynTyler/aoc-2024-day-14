e61f3bad589e1c50bdc16c222c943bc02c148268
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-14 18:06:43 +0200

    feat: stringifying robot struct

--- src/robot/robot.go
+++ src/robot/robot.go
@@ -1,6 +1,10 @@
 package robot
 
-import . "valyntyler.com/aoc-2024-day-14/src/vector2"
+import (
+	"fmt"
+
+	. "valyntyler.com/aoc-2024-day-14/src/vector2"
+)
 
 type Robot struct {
   position Vector2[uint]
@@ -9,4 +13,12 @@ type Robot struct {
 
 func NewRobot(position Vector2[uint], velocity Vector2[int]) Robot {
   return Robot { position, velocity }
+}
+
+func (r Robot) String() string {
+  return fmt.Sprintf(
+    "Robot:\n  position: %v\n  velocity: %v",
+    r.position,
+    r.velocity,
+  )
 }
