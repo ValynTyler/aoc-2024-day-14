8c0a73949c9c5f2da3f537747d2cc8f6d1623d11
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-14 18:22:17 +0200

    feat: parsing robot struct

--- src/bin/part1/main.go
+++ src/bin/part1/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
-	"strconv"
 
 	"valyntyler.com/aoc-2024-day-14/input"
-	. "valyntyler.com/aoc-2024-day-14/src/vector2"
 	. "valyntyler.com/aoc-2024-day-14/src/robot"
 )
 
@@ -20,22 +18,7 @@ func main() {
   var lines []string = strings.Split(result, "\n")
 
   for _, line := range lines {
-    var tokens []string = strings.Split(line, " ")
-
-    var pos_string = strings.Trim(tokens[0], "p=")
-    var vel_string = strings.Trim(tokens[1], "v=")
-
-    var pos_values = strings.Split(pos_string, ",")
-    pos_x, _ := strconv.ParseUint(pos_values[0], 10, 32)
-    pos_y, _ := strconv.ParseUint(pos_values[1], 10, 32)
-    pos := NewVector2(uint(pos_x), uint(pos_y))
-
-    var vel_values = strings.Split(vel_string, ",")
-    vel_x, _ := strconv.ParseInt(vel_values[0], 10, 32)
-    vel_y, _ := strconv.ParseInt(vel_values[1], 10, 32)
-    vel := NewVector2(int(vel_x), int(vel_y))
-
-    robot := NewRobot(pos, vel)
+    robot := ParseRobot(line)
     fmt.Println(robot)
   }
 }
--- src/robot/robot.go
+++ src/robot/robot.go
@@ -2,6 +2,8 @@ package robot
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	. "valyntyler.com/aoc-2024-day-14/src/vector2"
 )
@@ -13,6 +15,25 @@ type Robot struct {
 
 func NewRobot(position Vector2[uint], velocity Vector2[int]) Robot {
   return Robot { position, velocity }
+}
+
+func ParseRobot(s string) Robot {
+    var tokens []string = strings.Split(s, " ")
+
+    var pos_string = strings.Trim(tokens[0], "p=")
+    var vel_string = strings.Trim(tokens[1], "v=")
+
+    var pos_values = strings.Split(pos_string, ",")
+    pos_x, _ := strconv.ParseUint(pos_values[0], 10, 32)
+    pos_y, _ := strconv.ParseUint(pos_values[1], 10, 32)
+    pos := NewVector2(uint(pos_x), uint(pos_y))
+
+    var vel_values = strings.Split(vel_string, ",")
+    vel_x, _ := strconv.ParseInt(vel_values[0], 10, 32)
+    vel_y, _ := strconv.ParseInt(vel_values[1], 10, 32)
+    vel := NewVector2(int(vel_x), int(vel_y))
+
+    return NewRobot(pos, vel)
 }
 
 func (r Robot) String() string {
